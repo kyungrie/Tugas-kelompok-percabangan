369ef163c941ab57705fa870730cbd01ece303f7
Arie Ginanjar Januar <59875697+kyungrie@users.noreply.github.com>
2022-10-21 12:57:09 +0700

    Add files via upload

---
+++ kelompok progdas/Arie ginanjar januar/Tugas percabangan.php
@@ -0,0 +1,18 @@
+<?php
+$a = 80;
+$b = 81;
+
+if($a % 2 == 40){
+    echo 'a adalah bilangan ganjil';
+}
+else{
+    echo 'a adalah bilangan genap';
+}
+echo "<pre>";
+if($b % 2 ==40){
+    echo 'b adalah bilangan genap';
+}
+else{
+    echo 'b adalah bilangan ganjil';
+}
+?>
